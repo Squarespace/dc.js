13b60c845183578814a11b90da3343861faa3df5
Riyaz Shaikh <rshaikh@squarespace.com>
2012-10-12 16:53:33 -0400

    adding tableChart

--- src/table-chart.js
+++ src/table-chart.js
@@ -1,3 +1,4 @@
+
 dc.tableChart = function(selector, hierarchical) {
 
     var chart = dc.singleSelectionChart(dc.colorChart(dc.baseChart({})), hierarchical);
@@ -14,108 +15,41 @@ dc.tableChart = function(selector, hierarchical) {
     chart.transitionDuration(350);
 
     chart.render = function() {
-        chart.resetSvg();
+      chart.selectAll("div.row").remove();
 
-        if (chart.dataAreSet()) {
-            g = chart.svg()
-                .append("g")
-                .attr("transform", "translate(" + chart.cx() + "," + chart.cy() + ")");
+      if (chart.dataAreSet()) {
 
-            dataPie = calculateDataPie();
-            dataPieDimension = chart.dimension();
+        dataPie = calculateDataPie();
+        
+        var rowEnter = chart.root()
+          .selectAll("div.row")
+          .data(dataPie(chart.group().top(Infinity)))
+          .enter()
+          .append("div")
+          .attr("class", "row");
 
-            arc = chart.buildArcs();
 
-            slices = chart.drawSlices(g, dataPie, arc);
-
-            chart.drawLabels(slices, arc);
-            chart.drawTitles(slices, arc);
-
-            chart.highlightFilter();
+        var columns = [
+            function(d) {
+              return d.data.key;
+            },
+            function(d) {
+              return d.data.value;
+            }
+          ];
+        
+        for (var i = 0; i < columns.length; ++i) {
+              var f = columns[i];
+              rowEnter.append("span")
+                  .attr("class", "column column-" + i + (columns.length - i == 1 ? " last-column" : ""))
+                  .text(function(d) {
+                      return f(d);
+                  })
+                  .on('click', onClick);
         }
+      }
 
-        return chart;
-    };
-
-    chart.innerRadius = function(r) {
-        if (!arguments.length) return innerRadius;
-        innerRadius = r;
-        return chart;
-    };
-
-    chart.radius = function(r) {
-        if (!arguments.length) return radius;
-        radius = r;
-        return chart;
-    };
-
-    chart.cx = function() {
-        return chart.width() / 2;
-    };
-
-    chart.cy = function() {
-        return chart.height() / 2;
-    };
-
-    chart.buildArcs = function() {
-        return d3.svg.arc().outerRadius(radius).innerRadius(innerRadius);
-    };
-
-    chart.drawSlices = function(topG, dataPie, arcs) {
-        slices = topG.selectAll("g." + sliceCssClass)
-            .data(dataPie(chart.orderedGroup().top(Infinity)))
-            .enter()
-            .append("g")
-            .attr("class", function(d, i) {
-                return sliceCssClass + " " + i;
-            });
-
-        slicePaths = slices.append("path")
-            .attr("fill", function(d, i) {
-                return chart.colors()(i);
-            })
-            .attr("d", arcs);
-
-        slicePaths
-            .transition()
-            .duration(chart.transitionDuration())
-            .attrTween("d", tweenPie);
-
-        slicePaths.on("click", onClick);
-
-        return slices;
-    };
-
-    chart.drawLabels = function(slices, arc) {
-        if (chart.renderLabel()) {
-            labels = slices.append("text");
-            redrawLabels(arc);
-            labels.on("click", onClick);
-        }
-    };
-
-    chart.drawTitles = function(slices, arc) {
-        if (chart.renderTitle()) {
-            slices.append("title").text(function(d) {
-                return chart.title()(d);
-            });
-        }
-    };
-
-    chart.highlightFilter = function() {
-        if (chart.hasFilter()) {
-            chart.selectAll("g." + sliceCssClass).select("path").each(function(d) {
-                if (chart.isSelectedSlice(d)) {
-                    chart.highlightSelected(this);
-                } else {
-                    chart.fadeDeselected(this);
-                }
-            });
-        } else {
-            chart.selectAll("g." + sliceCssClass).selectAll("path").each(function(d) {
-                chart.resetHighlight(this);
-            });
-        }
+      return chart;
     };
 
     chart.isSelectedSlice = function(d) {
@@ -123,69 +57,27 @@ dc.tableChart = function(selector, hierarchical) {
     };
 
     chart.redraw = function() {
-    	if ( dataPieDimension !== chart.dimension() )
-    		return chart.render();
-        chart.highlightFilter();
-        var data = dataPie(chart.orderedGroup().top(Infinity));
-        slicePaths = slicePaths.data(data);
-        labels = labels.data(data);
-        dc.transition(slicePaths, chart.transitionDuration(), function(s) {
-            s.attrTween("d", tweenPie);
-        });
-        redrawLabels(arc);
-        redrawTitles();
-        return chart;
+      chart.render();
+
+      if (chart.hasFilter()) {
+          chart.selectAll("div.row").each(function(d) {
+              if (chart.isSelectedSlice(d)) {
+                  chart.highlightSelected(this);
+              } else {
+                  chart.fadeDeselected(this);
+              }
+          });
+      } else {
+          chart.selectAll("div.row").each(function(d) {
+              chart.resetHighlight(this);
+          });
+      }
     };
 
     function calculateDataPie() {
         return d3.layout.pie().value(function(d) {
             return chart.valueRetriever()(d);
         });
-    }
-
-    function redrawLabels(arc) {
-        dc.transition(labels, chart.transitionDuration())
-            .attr("transform", function(d) {
-                d.innerRadius = chart.innerRadius();
-                d.outerRadius = radius;
-                var centroid = arc.centroid(d);
-                if (isNaN(centroid[0]) || isNaN(centroid[1])) {
-                    return "translate(0,0)";
-                } else {
-                    return "translate(" + centroid + ")";
-                }
-            })
-            .attr("text-anchor", "middle")
-            .text(function(d) {
-                var data = d.data;
-                if (chart.valueRetriever()(data) == 0)
-                    return "";
-                return chart.label()(d);
-            });
-    }
-
-    function redrawTitles() {
-        if (chart.renderTitle()) {
-            slices.selectAll("title").text(function(d) {
-                return chart.title()(d);
-            });
-        }
-    }
-
-    function tweenPie(b) {
-        b.innerRadius = chart.innerRadius();
-        var current = this._current;
-        if (isOffCanvas(current))
-            current = {startAngle: 0, endAngle: 0};
-        var i = d3.interpolate(current, b);
-        this._current = i(0);
-        return function(t) {
-            return arc(i(t));
-        };
-    }
-
-    function isOffCanvas(current) {
-        return current == null || isNaN(current.startAngle) || isNaN(current.endAngle);
     }
 
     function onClick(d) {
